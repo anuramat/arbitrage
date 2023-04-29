0be58614091299b06c509f5a9b641a7211e1d03b
Arsen Nuramatov <anuramat@pm.me>
2023-04-29 17:45:08 +0600

    kinda finished for now

--- internal/strategy/detection.go
+++ internal/strategy/detection.go
@@ -5,25 +5,23 @@ import (
 	"time"
 
 	"github.com/anuramat/arbitrage/internal/models"
+	"github.com/shopspring/decimal"
 )
 
 func DetectArbitrage(allMarkets *models.AllMarkets) {
 	counter := 0
 	fmt.Println("Waiting for exchanges to start...")
-	time.Sleep(3 * time.Second)
+	time.Sleep(5 * time.Second)
 	for {
 		time.Sleep(1 * time.Second)
 		counter++
 		fmt.Printf("Checking for arbitrage opportunities... (%d)\n", counter)
+		highestProfit := decimal.NewFromInt(0)
+		highestProfitPair := ""
 		for currencyPair, markets := range *allMarkets {
-
 			markets[0].BestPrice.RLock()
 			highestBid := markets[0].BestPrice.Bid
 			lowestAsk := markets[0].BestPrice.Ask
-			if currencyPair == "TUSD_USDT" {
-				fmt.Println(markets[0].BestPrice.Bid)
-				fmt.Println(markets[0].BestPrice.Ask)
-			}
 			markets[0].BestPrice.RUnlock()
 			for _, market := range markets[1:] {
 				market.BestPrice.RLock()
@@ -33,12 +31,20 @@ func DetectArbitrage(allMarkets *models.AllMarkets) {
 				if market.BestPrice.Ask.LessThan(lowestAsk) {
 					lowestAsk = market.BestPrice.Ask
 				}
-				if currencyPair == "TUSD_USDT" {
-					fmt.Println(markets[1].BestPrice.Bid)
-					fmt.Println(markets[1].BestPrice.Ask)
-				}
 				market.BestPrice.RUnlock()
 			}
+			// calculate profit
+			if highestBid.IsZero() || lowestAsk.IsZero() {
+				continue
+			}
+			profit := highestBid.Sub(lowestAsk)
+			profit = profit.Div(lowestAsk.Add(highestBid))
+			if profit.GreaterThan(highestProfit) {
+				highestProfit = profit
+				highestProfitPair = currencyPair
+			}
+
 		}
+		fmt.Printf("Highest profit: %s on %s\n", highestProfit.StringFixed(8), highestProfitPair)
 	}
 }
